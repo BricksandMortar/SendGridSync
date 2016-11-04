3f90d3662e7dbe8955938255b5449187a5a6b152
Arran France <arranf@gmail.com>
2016-11-04 22:12:10 +0000

    Bump to 900 added per request

--- Constants/SendGridRequest.cs
+++ Constants/SendGridRequest.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace com.bricksandmortarstudio.SendGridSync.Constants
+﻿namespace com.bricksandmortarstudio.SendGridSync.Constants
 {
     public class SendGridRequest
     {
         public const string SENDGRID_BASE_URL = "https://api.sendgrid.com/v3/";
         public const string RECIPIENTS_RESOURCE = "contactdb/recipients";
         public const string CUSTOM_FIELDS_RESOURCE = "contactdb/custom_fields";
-        public const int SENDGRID_ADD_RECEIPIENT_MAX_COUNT = 500;
+        public const int SENDGRID_ADD_RECEIPIENT_MAX_COUNT = 900;
         public const string LISTS_RESOURCE = "/contactdb/lists";
         public const string LISTS_PERSON_RESOUCE = "recipients";
     }
