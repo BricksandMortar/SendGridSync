925ae0ba70c560e5d5ca792356f969fc25eeca7c
Arran France <arranf@gmail.com>
2016-11-04 22:16:18 +0000

    Add ForeignKey to field

---
+++ Migrations/002_AddForeignKey.cs
@@ -0,0 +1,20 @@
+﻿using Rock.Plugin;
+
+namespace com.bricksandmortarstudio.SendGridSync.Migrations
+{
+    [MigrationNumber( 2, "1.5.0" )]
+    public class AddForeignKey : Migration
+    {
+        public override void Up()
+        {
+            Sql( @"ALTER TABLE [_com_bricksandmortarstudio_SendGridSync_PersonAliasHistory]
+ADD FOREIGN KEY ([PersonAliasId])
+REFERENCES [PersonAlias]([Id])" );
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
