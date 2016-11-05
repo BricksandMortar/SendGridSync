1b02742d130f5d44dad5ba417e55c1522675619a
Arran France <arranf@gmail.com>
2016-11-05 14:33:34 +0000

    Ensure index set

---
+++ Migrations/003_AddIndex.cs
@@ -0,0 +1,19 @@
+﻿using Rock.Plugin;
+
+namespace com.bricksandmortarstudio.SendGridSync.Migrations
+{
+    [MigrationNumber( 3, "1.5.0" )]
+    public class AddIndex : Migration
+    {
+        public override void Up()
+        {
+            Sql( @"CREATE INDEX IX_PersonAliasId
+                    ON [_com_bricksandmortarstudio_SendGridSync_PersonAliasHistory] ([PersonAliasId])" );
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
